0c61e81f770e310740d881a1c9ebe194afa64620
Mridul Pandey <pandeym891@gmail.com>
2025-06-13 19:47:51 +0530

    Completed till zod validation

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,18 +1,35 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "./config.js";
+
 import { middleware } from "./middleware.js";
+import { JWT_SECRET } from '@repo/backend-common/config';
+import { CreateRoomSchema, CreateUserSchema, SignInSchema } from "@repo/common/types";
+
 const app = express();
 
 app.use(cors);
 
 app.post("/signup", (req, res) => {
+  const data= CreateUserSchema.safeParse(req.body)
+  if (!data){
+     res.json({
+      message : "Incorrect Inputs"
+    })
+    return;
+  }
     res.json({
         userId :"123"
     })
 });
 
 app.post("/signin", (req, res) => {
+  const data= SignInSchema.safeParse(req.body)
+  if (!data){
+     res.json({
+      message : "Incorrect Inputs"
+    })
+    return;
+  }
   const userId = 1;
   const token = jwt.sign({ userId }, JWT_SECRET);
   res.json({
@@ -20,5 +37,16 @@ app.post("/signin", (req, res) => {
   });
 });
 
-app.post("/room", middleware,(req, res) => {});
+app.post("/room", middleware,(req, res) => {
+  const data= CreateRoomSchema.safeParse(req.body)
+  if (!data){
+     res.json({
+      message : "Incorrect Inputs"
+    })
+    return;
+  }
+  res,json({
+    roomId : 123
+  })
+});
 app.listen(3001);
--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,5 +1,5 @@
 import { NextFunction } from "express";
-import { JWT_SECRET } from "./config.js";
+import { JWT_SECRET } from "@repo/backend-common/config";
 import jwt from "jsonwebtoken";
 export function middleware ( req :Request, res:Response  , next:NextFunction ) {
         const token = req.headers["authorization"] ?? "";
