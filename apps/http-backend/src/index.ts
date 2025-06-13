0b60c57b49a3b60f9a41cede7eb2abbe66bfbc46
Mridul Pandey <pandeym891@gmail.com>
2025-06-13 19:17:23 +0530

    Creation of ugly backend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,24 @@
 import express from "express";
-
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config.js";
+import { middleware } from "./middleware.js";
 const app = express();
 
+app.use(cors);
+
+app.post("/signup", (req, res) => {
+    res.json({
+        userId :"123"
+    })
+});
+
+app.post("/signin", (req, res) => {
+  const userId = 1;
+  const token = jwt.sign({ userId }, JWT_SECRET);
+  res.json({
+    token,
+  });
+});
+
+app.post("/room", middleware,(req, res) => {});
 app.listen(3001);
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,11 +1,24 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer } from "ws";
+import jwt, {  JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "./config.js";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
+wss.on("connection", function connection(ws, request) {
+  const url = request.url;
+  if (!url) {
+    return;
+  }
 
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
+  const queryParams = new URLSearchParams(url.split("?")[1]);
+  const token = queryParams.get("token") || "";
+
+  const decoded = jwt.verify(token, JWT_SECRET);
+  if (!decoded || !(decoded as JwtPayload).userId) {
+    ws.close();
+    return;
+  }
+  ws.on("message", function message(data) {
+    console.log("received: %s", data);
   });
-
 });
